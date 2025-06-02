fbb2bb738c2dcffbadd2f850e3105ae6b5dd5c4f
Achmad <achmadsyahhidayat@gmail.com>
2025-06-03 04:54:18 +0700

    test migrasi kedua

---
+++ mynextjs-coffeeweb/app/header.js
@@ -0,0 +1,7 @@
+export default function Header() {
+	return (
+		<div>
+			<h1>Coffee Time</h1>
+		</div>
+	);
+}
---
+++ mynextjs-coffeeweb/app/home/page.js
@@ -0,0 +1,8 @@
+export default function CoffeeTime() {
+	return (
+		<main>
+			<h1>Coffee Time</h1>
+			<a href="/">Back to Home</a>
+		</main>
+	);
+}
